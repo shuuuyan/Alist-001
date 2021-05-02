112d55e25df0c6dd5c0cd0d40f3092386c981a0d
shuuuyan <shuchan1023@gmail.com>
2021-05-02 19:12:54 +0900

    firstcommit

--- app/models/alist.rb
+++ app/models/alist.rb
@@ -3,7 +3,6 @@ class Alist < ApplicationRecord
   validates :list1, presence: true
   validates :list2, presence: true 
   validates :list3, presence: true 
-
   belongs_to :user
   has_many :comments, dependent: :destroy
 end
