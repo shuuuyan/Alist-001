0db933d02278b5957a139d88ab1de959e8a7fe55
shuuuyan <shuchan1023@gmail.com>
2021-05-03 18:36:56 +0900

    同期いいねの実装

--- app/models/alist.rb
+++ app/models/alist.rb
@@ -5,4 +5,5 @@ class Alist < ApplicationRecord
   validates :list3, presence: true 
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-
   has_many :alists
   has_many :comments
+  has_many :likes
+
+  def liked_by?(alist_id)
+    likes.where(alist_id: alist_id).exists?
+  end
 end
