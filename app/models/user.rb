532d54861caec51c2da479f0f36165ac32bf4cd3
shuuuyan <shuchan1023@gmail.com>
2021-05-19 12:26:43 +0900

    バリデーションの追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 7 }, format: {with: /\A[a-zA-Z]+\z/ }
   has_many :alists
   has_many :comments
   has_many :likes
